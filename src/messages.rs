6c3335989f781c786f968718361e68abd1cae23f
Ayke van Laethem <aykevanlaethem@gmail.com>
2016-10-12 17:25:35 +0200

    cargo fmt

--- src/messages.rs
+++ src/messages.rs
@@ -8,6 +8,9 @@ fn test_color_conversion() {
     let color_raw = 0x4148ffff;
     let color = Color::from_raw(color_raw);
     if color.raw() != color_raw {
-        panic!("color {:08x} turns into {:08x} after conversion to {:?}", color_raw, color.raw(), color);
+        panic!("color {:08x} turns into {:08x} after conversion to {:?}",
+               color_raw,
+               color.raw(),
+               color);
     }
 }
